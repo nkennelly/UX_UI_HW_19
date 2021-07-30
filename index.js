7e6cf88575b642e7c25ddf270935f0e18a2c2505
nkennelly <natekennelly@gmail.com>
2021-07-29 21:26:36 -0400

    Final Week 19

--- index.js
+++ index.js
@@ -7,11 +7,18 @@
 $("#WECYCLEbutton").on("click", function() {
     console.log("Hey a click occurred!");
   });
+
+
+$("#menu").on("click", function() {
+    document.getElementById("myNav").style.width = "100%";
+  });
+
+  $("#closebutton").on("click", function() {
+    document.getElementById("myNav").style.width = "0%";
+  });
   
-
-$("#myWorklink").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#myWork").offset().top
-    }, 2000);
-});
-
+// $("#myWorklink").click(function() {
+//     $('html, body').animate({ 
+//         scrollTop: $("#myWork").offset().top
+//     }, 2000);
+// });
